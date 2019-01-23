2b6b158fa1533b7ba3f1dccb61dcd00f04a4214f
sraveendras <srks9441@gmail.com>
2019-01-23 15:49:25 +0530

    My First Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup(){
+		
+		System.setProperty("WebDriver.chrome.driver", "F:\\FrameworkProject\\MyProject\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+	
+	
+	@Test
+	public void dologin(){
+		
+		driver.get("https://www.zoho.com/");
+		driver.findElement(By.cssSelector(".zh-login")).click();
+	}
+	
+	
+	@AfterSuite
+	public void closed(){
+		
+		driver.close();
+	}
+
+}
